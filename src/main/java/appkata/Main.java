a9d93416c8b360a6b65a9817108d0b6f0856d448
Claus Polanka <claus.polanka@chello.at>
2011-08-19 21:15:18 +0200

    Extracted some methods and moved some packages

--- src/main/java/appkata/Main.java
+++ src/main/java/appkata/Main.java
@@ -7,11 +7,15 @@ import static java.lang.System.lineSeparator;
 
 public class Main {
     private static final int ARG_FILE_PATH = 0;
-
+    private static final String FILE_ENCODING = "UTF-8";
     public static StringBuilder fileContent = new StringBuilder();
 
     public static void main(String... args) throws Exception {
-        Scanner scanner = new Scanner(file(args[ARG_FILE_PATH]), "UTF-8");
+        new CsvFile(readFileContentOf(args[ARG_FILE_PATH]));
+    }
+
+    public static String readFileContentOf(String filePath) {
+        Scanner scanner = new Scanner(file(filePath), FILE_ENCODING);
         try {
             while (scanner.hasNextLine()) {
                 fileContent.append(scanner.nextLine() + lineSeparator());
@@ -19,7 +23,7 @@ public class Main {
         } finally {
             scanner.close();
         }
-        new CsvFile(fileContent.toString().trim());
+        return fileContent.toString().trim();
     }
 
     private static InputStream file(String filePath) {
