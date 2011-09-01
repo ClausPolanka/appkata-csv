01e16857561b012411200dfa0cfaec66d746c6f4
Claus Polanka <claus.polanka@chello.at>
2011-09-02 00:02:30 +0200

    Added display dependency and mocked it in tests via jmock

--- src/test/unit/java/test/unit/appkata/CsvViewerTest.java
+++ src/test/unit/java/test/unit/appkata/CsvViewerTest.java
@@ -2,73 +2,80 @@ package test.unit.appkata;
 
 import appkata.CsvViewer;
 import appkata.Display;
+import org.jmock.Expectations;
+import org.jmock.auto.Mock;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
 import org.junit.Test;
-
 import static java.lang.System.lineSeparator;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
 
 public class CsvViewerTest {
-    private static final Display IGNORE_DISPLAY = null;
+    @Mock
+    private Display display;
+    @Rule
+    public JUnitRuleMockery context = new JUnitRuleMockery();
 
     @Test
     public void viewOutputContainingHeaderWithOneColumnForGivenCsvFileContent() throws Exception {
-        CsvViewer csvViewer = new CsvViewer(IGNORE_DISPLAY);
-        String fileContent = "Name;" + lineSeparator();
-        csvViewer.view(fileContent);
-        String expectedViewerOutput = "Name|" + lineSeparator() +
-                                      "----+";
-        assertThat("Generated viewer output", csvViewer.output(), is(equalTo(expectedViewerOutput)));
+        context.checking(new Expectations() {{
+            oneOf(display).print("Name|" + lineSeparator() +
+                                 "----+");
+        }});
+        CsvViewer csvViewer = new CsvViewer(display);
+        csvViewer.view("Name;" + lineSeparator());
     }
 
     @Test
     public void viewOutputContainingHeaderWithSeveralColumnsForGivenCsvFileContent() throws Exception {
-        CsvViewer csvViewer = new CsvViewer(IGNORE_DISPLAY);
+        context.checking(new Expectations() {{
+            oneOf(display).print("Name|Age|City|" + lineSeparator() +
+                                 "----+---+----+");
+        }});
+        CsvViewer csvViewer = new CsvViewer(display);
         String fileContent = "Name;Age;City;" + lineSeparator();
         csvViewer.view(fileContent);
-        String expectedViewerOutput = "Name|Age|City|" + lineSeparator() +
-                                      "----+---+----+";
-        assertThat("Generated viewer output", csvViewer.output(), is(equalTo(expectedViewerOutput)));
     }
 
     @Test
     public void viewOutputContainingHeaderWithOneColumnAndOneTableLineForGivenCsvFileContent() throws Exception {
-        CsvViewer csvViewer = new CsvViewer(IGNORE_DISPLAY);
+        context.checking(new Expectations() {{
+            oneOf(display).print("Name |" + lineSeparator() +
+                                 "-----+" + lineSeparator() +
+                                 "Peter|");
+        }});
+        CsvViewer csvViewer = new CsvViewer(display);
         String fileContent = "Name;" + lineSeparator() +
                              "Peter;";
         csvViewer.view(fileContent);
-        String expectedViewerOutput = "Name |" + lineSeparator() +
-                                      "-----+" + lineSeparator() +
-                                      "Peter|";
-        assertThat("Generated viewer output", csvViewer.output(), is(equalTo(expectedViewerOutput)));
     }
 
     @Test
     public void viewOutputContainingHeaderWithSeveralColumnsAndOneTableLineForGivenCsvFileContent() throws Exception {
-        CsvViewer csvViewer = new CsvViewer(IGNORE_DISPLAY);
+        context.checking(new Expectations() {{
+            oneOf(display).print("Name |Age|City    |" + lineSeparator() +
+                                 "-----+---+--------+" + lineSeparator() +
+                                 "Peter|42 |New York|");
+        }});
+        CsvViewer csvViewer = new CsvViewer(display);
         String fileContent = "Name;Age;City;" + lineSeparator() +
                              "Peter;42;New York;";
         csvViewer.view(fileContent);
-        String expectedViewerOutput = "Name |Age|City    |" + lineSeparator() +
-                                      "-----+---+--------+" + lineSeparator() +
-                                      "Peter|42 |New York|";
-        assertThat("Generated viewer output", csvViewer.output(), is(equalTo(expectedViewerOutput)));
     }
 
     @Test
     public void viewOutputContainingHeaderWithSeveralColumnsAndSeveralTableLinesForGivenCsvFileContent() throws Exception {
-        CsvViewer csvViewer = new CsvViewer(IGNORE_DISPLAY);
+        context.checking(new Expectations() {{
+            oneOf(display).print("Name |Age|City    |" + lineSeparator() +
+                                 "-----+---+--------+" + lineSeparator() +
+                                 "Peter|42 |New York|" + lineSeparator() +
+                                 "Paul |57 |London  |" + lineSeparator() +
+                                 "Mary |35 |Munich  |");
+        }});
+        CsvViewer csvViewer = new CsvViewer(display);
         String fileContent = "Name;Age;City;" + lineSeparator() +
                              "Peter;42;New York;" + lineSeparator() +
                              "Paul;57;London;" + lineSeparator() +
                              "Mary;35;Munich;";
         csvViewer.view(fileContent);
-        String expectedViewerOutput = "Name |Age|City    |" + lineSeparator() +
-                                      "-----+---+--------+" + lineSeparator() +
-                                      "Peter|42 |New York|" + lineSeparator() +
-                                      "Paul |57 |London  |" + lineSeparator() +
-                                      "Mary |35 |Munich  |";
-        assertThat("Generated viewer output", csvViewer.output(), is(equalTo(expectedViewerOutput)));
     }
 }
