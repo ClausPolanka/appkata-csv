857fd55b802ee02351a626d42227d14e378b150f
Claus Polanka <claus.polanka@chello.at>
2012-01-06 21:28:04 +0100

    Refactoring: Extracted some helper classes and methods to separate concerns and apply single responsibility principle.

---
+++ src/test/integration/java/ProcessBuilderTest.java
@@ -0,0 +1,41 @@
+import org.junit.AfterClass;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.Scanner;
+
+import static appkata.Main.FILE_ENCODING;
+import static java.lang.System.lineSeparator;
+import static org.apache.commons.io.FileUtils.deleteQuietly;
+import static org.apache.commons.io.FileUtils.writeStringToFile;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ProcessBuilderTest {
+    private static final String FILE_NAME = "simple.bat";
+    private static final String EXPECTED_OUTPUT = "Hello World";
+    private static final File CURRENT_DIRECTORY = new File(".");
+
+    @Test
+    public void executeSelfMadeBatchFileAndReadOutput() throws Exception {
+        writeStringToFile(new File(FILE_NAME), "@echo off" + lineSeparator() +
+                                               "echo " + EXPECTED_OUTPUT, FILE_ENCODING);
+
+        Process batchFile = new ProcessBuilder(FILE_NAME)
+                            .directory(CURRENT_DIRECTORY)
+                            .start();
+
+        assertThat("Batch output", outputOf(batchFile), is(equalTo(EXPECTED_OUTPUT)));
+    }
+
+    private String outputOf(Process p) {
+        Scanner s = new Scanner(p.getInputStream()).useDelimiter("\\Z");
+        return s.next();
+    }
+
+    @AfterClass
+    public static void deleteBatchFile() {
+        deleteQuietly(new File(FILE_NAME));
+    }
+}
