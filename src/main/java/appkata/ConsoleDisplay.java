3375b977999ea96b6c27975d4630cee96ca05ee7
Claus Polanka <claus.polanka@chello.at>
2012-01-23 00:05:49 +0100

    Fixed bug in end-to-end test. Display now takes Array of pages instead of only one page.

--- src/main/java/appkata/ConsoleDisplay.java
+++ src/main/java/appkata/ConsoleDisplay.java
@@ -1,7 +1,9 @@
 package appkata;
 
 public class ConsoleDisplay implements Display {
-    public void view(String... result) {
-        System.out.println(result);
+    public void view(String... pages) {
+        for (String p : pages) {
+            System.out.println(p);
+        }
     }
 }
