43c7b7e94c4aeb16b35087f791e140af8cba46d0
Claus Polanka <claus.polanka@chello.at>
2012-04-05 20:31:23 +0200

    Stopped implementing the sniper

--- src/main/java/goos/Auction.java
+++
@@ -1,8 +0,0 @@
-package goos;
-
-import org.jivesoftware.smack.XMPPException;
-
-public interface Auction {
-    void bid(int bid);
-    void join() throws XMPPException;
-}
--- src/main/java/goos/AuctionEventListener.java
+++
@@ -1,9 +0,0 @@
-package goos;
-
-public interface AuctionEventListener {
-    enum PriceSource {
-        FromSniper, FromOtherBidder;
-    };
-    void auctionClosed();
-    void currentPrice(int price, int increment, PriceSource priceSource);
-}
